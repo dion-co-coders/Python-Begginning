c2f8ed7dda1addc8764fb3a6bfc997d4cd9fc159
Dion JOHNSON <51814524+DJcoder007@users.noreply.github.com>
2020-12-09 12:48:17 +0530

    Add files via upload

---
+++ class 01.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Hai Everyone..... Welcome JUPYTER book
+
+# In[7]:
+
+
+print("Hallo guys,I am New to python here")#printing statement in jupyter book.
+
+
+# In[8]:
+
+
+print("Hallo guys,I am New to python here")print("Hallo guys,I am New to python here")
+
+
+# In[9]:
+
+
+"Hallo guys,I am New to python here");print("Hallo guys,I am New to python here")
+
+
+# In[10]:
+
+
+print("Hallo guys,I am New to python here");print("Hallo guys,\nI am New to python here")
+
+
+# In[11]:
+
+
+print("""1
+2
+3
+4
+5""")
+
+
+# In[12]:
+
+
+print("Hallo guys,I am New to python here")
+print("Hallo guys,I am New to python here")
+#1print("Hallo guys,I am New to python here")
+#2print("Hallo guys,I am New to python here")
+
+
+# # Variables and Datatypes
+
+# In[15]:
+
+
+a = 77
+
+
+# In[16]:
+
+
+type(a)
+
+
+# In[19]:
+
+
+ha = 
+
+
+# In[23]:
+
+
+type(ha)
+
+
+# In[24]:
+
+
+isthisaname = "DION JOHNSON"
+
+
+# In[25]:
+
+
+type(isthisaname)
+
+
+# In[ ]:
+
+
+
+
